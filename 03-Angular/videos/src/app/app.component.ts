9aab693b0bbe89da09c4d66662a408c4a700a51b
DanyJos3 <jose196.jm@hotmail.com>
2018-11-12 17:58:35 -0500

    Instalación npm

---
+++ 03-Angular/videos/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import {} from 'http-server';
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'videos';
+}
